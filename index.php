d0ce0aaf690d618fd6070d2fc306af216c4a44fb
Quynh Nguyen <quynhnguyen98>
2020-04-28 19:44:37 +0700

    bo revert

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 =======
     <title>LawnCare - Free Bootstrap 4 Template by Colorlib</title>
     <title>LawnCare - Free Bootstrap 4 Template by Colorlib</title>
+<<<<<<< HEAD:index.php
+>>>>>>> parent of bbdfa4c... thua dong title:index.html
+=======
 >>>>>>> parent of bbdfa4c... thua dong title:index.html
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
