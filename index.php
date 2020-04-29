0531f35a1ad4ac525283afd4cabe6cbc00d0d99b
quynhnguyen98 <64259779+quynhnguyen98@users.noreply.github.com>
2020-04-29 12:41:26 +0700

    xoa bot title

--- index.php
+++ index.php
@@ -2,15 +2,7 @@
 <html lang="en">
 
 <head>
-<<<<<<< HEAD:index.php
     <title>Đây là trang web gia phả</title>
-=======
-    <title>LawnCare - Free Bootstrap 4 Template by Colorlib</title>
-    <title>LawnCare - Free Bootstrap 4 Template by Colorlib</title>
-<<<<<<< HEAD:index.php
->>>>>>> parent of bbdfa4c... thua dong title:index.html
-=======
->>>>>>> parent of bbdfa4c... thua dong title:index.html
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
